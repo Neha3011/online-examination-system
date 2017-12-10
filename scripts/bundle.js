a9da816a4c1fe554362ad1b5fb58134c59653ea7
Neha Singh <nehasingh301192@gmail.com>
2017-12-10 20:22:58 +0530

    Allow upgrad image

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "cbfd4c3cc39eb3157efe"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "630c92b956ae1962db36"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
